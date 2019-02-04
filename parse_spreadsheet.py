eaf90c4d61069df3c2b5cc0bb54a47784d5b34ab
Christine Phan <cxphan@Christines-MacBook-Pro-7.local>
2019-02-04 16:20:33 -0500

    Still need to fix TSV

--- parse_spreadsheet.py
+++ parse_spreadsheet.py
@@ -1,13 +1,23 @@
 # run with python2
+# for CS 230 
 import urllib2
 import re
 import csv
+import os
 
 # to implement --- convert into TSV file. 
 def makeLine(filename, obj):
-	with open('records.tsv', 'w') as tsvfile:
-    	writer = csv.writer(tsvfile, delimiter='\t', newline='\n')
-    	writer.writerow(obj.)
+	edited_obj = []
+	for value in obj.values():
+		edited_obj.append(value.encode('utf-8'))
+
+	with open(filename, 'w') as tsvfile:
+		writer = csv.writer(tsvfile, delimiter='\t', lineterminator='\n')
+		writer.writerow(['1'] + edited_obj)
+		writer.writerow(['2'] + edited_obj)
+		writer.writerow(['3'] + edited_obj)
+
+	tsvfile.close()
 
 # converts into object/dictionary form
 def createObject(entry): 
@@ -19,11 +29,17 @@ def createObject(entry):
 	matches = re.findall(pattern, entry)
 	for elem in matches: 
 		obj[elem[0]] = elem[1]
+
+	print obj
 	return obj
 
 
 
 def main():
+	filename = "records.tsv"
+	if os.path.exists(filename):
+  		os.remove(filename)
+
 	print "Running Main ... "
 	
 
@@ -32,8 +48,12 @@ def main():
 	pattern = "\n([\w\W\s]+?)\n\n"
 	matches = re.findall(pattern, data)
 
-	for entry in matches:
+
+	for i, entry in enumerate(matches):
 		obj = createObject(entry)
+		makeLine(filename, obj)
+		if i == 2:
+			break
 
 
 	
