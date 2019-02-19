2cb6794a6ecad112783b4248e3b5ae2dbbb7a71e
Christine Phan <cxphan@stanford.edu>
2019-02-19 13:53:33 -0500

    Save for submission

--- CS_230_project.py
+++ CS_230_project.py
@@ -3,8 +3,8 @@
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from keras.models import Model
@@ -14,33 +14,49 @@ from keras.preprocessing.text import Tokenizer
 from keras.preprocessing import sequence
 from keras.utils import to_categorical
 from keras.callbacks import EarlyStopping
-#%matplotlib inline
-df = pd.read_csv("small_fic_dataset.tsv",delimiter='\t',encoding='latin-1')
+%matplotlib inline
+
+# import data from small_fic_dataset.tsv 
+filename = "./data/small_fic_dataset.tsv"
+
+df = pd.read_csv(filename,delimiter='\t',encoding='latin-1')
 df.head()
-#df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'],axis=1,inplace=True)
+
+# here: dropping columns that are not required 
+df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'],axis=1,inplace=True)
 df.info()
+
+# visualizing distribution of data here 
 sns.countplot(df.MAINTAG)
 plt.xlabel('Label')
 plt.title('Number of fics in each rating')
 plt.show()
+
+# Creating input and output vectors, processing the labels.
 X = df.TEXT
 Y = df.MAINTAG
 le = LabelEncoder()
 Y = le.fit_transform(Y)
 Y = Y.reshape(-1,1)
+
+# split into training, test data 
 X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2,shuffle=True)
 print(Y_train)
 Y_train = to_categorical(Y_train, num_classes=4, dtype='float32')
 print(Y_train)
 Y_test = to_categorical(Y_test, num_classes=4, dtype='float32')
-#X_train=X
-#Y_train=Y
+X_train=X
+Y_train=Y
+
+# tokenize the words in the story text here 
 max_words = 5000
 max_len = 220
 tok = Tokenizer(num_words=max_words)
 tok.fit_on_texts(X_train)
 sequences = tok.texts_to_sequences(X_train)
 sequences_matrix = sequence.pad_sequences(sequences,maxlen=max_len)
+
+# RNN model 
 def RNN():
     inputs = Input(name='inputs',shape=[max_len])
     layer = Embedding(max_words,500,input_length=max_len)(inputs)
@@ -52,6 +68,7 @@ def RNN():
     layer = Activation('softmax')(layer)
     model = Model(inputs=inputs,outputs=layer)
     return model
+    
 model = RNN()
 model.summary()
 model.compile(loss='categorical_crossentropy',optimizer=RMSprop(),metrics=['accuracy'])
--- README.md
+++ README.md
@@ -13,5 +13,8 @@ These two variables describe the number of pages you are pulling from. For the f
 python parse_spreadsheet.py [Var1] [Var2]
 These two variables are the same as the ones you inputted above. 
 
-You can access this in the ./data folder. 
+You can access both the pre and post-parsed data in the ./data folder. 
 
+3. To run the main file, run:
+python CS_230_project.py 
+To run any other file in the data folder (records:X-Y.tsv), change the name of the filename. 
